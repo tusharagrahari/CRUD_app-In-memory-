6b420d865a51ad43bc52b47d496bdbabce601f15
Tushar Agrahari <tushar84_@Tushars-MacBook-Air.local>
2025-04-24 08:57:36 +0530

    First Commit

---
+++ src/main.rs
@@ -0,0 +1,34 @@
+use actix_web::{get, Responder, HttpServer};
+use serde::Serialize;
+
+
+#[derive(Serialize)]
+pub struct GenericResponse {
+    pub status: String,
+    pub message: String,
+}
+
+#[get("/health_check")]
+pub async fn health_check_handler() -> impl Responder {
+    let response = GenericResponse {
+        status: "success".to_string(),
+        message: "Server is running".to_string(),
+    };
+    actix_web::HttpResponse::Ok().json(response)
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    println!("Server starting on port 8080");
+
+    HttpServer::new(|| {
+        println!("Server is running");
+        actix_web::App::new()
+            .service(health_check_handler)
+    })
+    .bind("127.0.0.1:8080")?
+    .run()
+    .await
+}
+
+
